d57df1f93188925d3048f869143f8242ad38663c
user.email <43427322+PacoMedin@users.noreply.github.com>
2020-03-03 00:45:12 -0600

    Listar registros

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.mascota.views import index_mascota, mascota_view
+from apps.mascota.views import index_mascota, mascota_view, mascota_list
 
 urlpatterns = [
 
     path('', index_mascota, name='index'),
     path('nuevo', mascota_view, name='mascota_crear'),
+    path('listar', mascota_list, name = 'mascota_listar')
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apps.mascota.forms import MascotaForm
+from apps.mascota.models import Mascota
 # Create your views here.
 """def index_mascota(request):
     return HttpResponse("Index mascota")"""
@@ -17,3 +18,8 @@ def mascota_view(request):
     else :
         form = MascotaForm()
     return render(request, 'mascota/mascota_form.html', {'form': form})
+
+def mascota_list(request):
+    mascota = Mascota.objects.all()
+    context = {"mascotas" : mascota}
+    return render(request, 'mascota/mascota_list.html', context)
